5593b2a37a2551b43eea4088dc79d4bc4272e4f6
Burnings <Visions-Chen@hotmail.com>
2015-02-25 14:53:58 +0800

    Revert "Revert "Revert "project add!"""
    This reverts commit d8b1496ac6e697e7ea027a3ccc09b48d8abf59f7.

--- src/com/qcyg/jochen/esheng/widget/autocomp/internal/filter/AccountFilter.java
+++
@@ -1,8 +0,0 @@
-package com.qcyg.jochen.esheng.widget.autocomp.internal.filter;
-
-import android.accounts.Account;
-
-
-public interface AccountFilter {
-    public String filter(Account account);
-}
